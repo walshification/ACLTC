e82984523378b67a172624efd801d2a4571a9743
walshification <walshification@gmail.com>
2014-09-08 08:06:50 -0500

    screencast 01
    Watched and completed the exercise following the first screencast

---
+++ screencast 01.rb
@@ -0,0 +1,11 @@
+# The difference between the following:
+
+# "h" + "a" * 10
+
+# ("h" + "a") * 10
+
+# is which letter the 10 is applied to. In the first instance, the 10 only 
+# applies to the "a", giving us the output "haaaaaaaaaa". In the second, the 
+# 10 is applied to both letters, giving us the output "hahahahahahahahahaha". 
+# The parentheses in the second example alters the order of operations for the
+# concatenation of the strings.
