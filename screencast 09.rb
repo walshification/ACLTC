e33753a672a836595599c5c7b378864a68c3e332
walshification <walshification@gmail.com>
2014-09-08 21:23:01 -0500

    screencast 09
    A word-chooser program.

---
+++ screencast 09.rb
@@ -0,0 +1,13 @@
+# A word-chooser program.
+puts "Welcome to the Word Chooser! Please enter four words, one at a time."
+
+words = []
+
+4.times do
+	puts "Enter a word:"
+	words << gets.chomp
+end
+
+puts "Now, enter a number between 1 and 4:"
+choice = gets.chomp.to_i - 1
+puts words[choice]
